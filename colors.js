530f0e0dfa7b039b5dc8f57ff8a97efee44ed103
“naandalist” <listiananda.apriliawan@gmail.com>
2023-01-03 00:10:48 +0700

    feat: add colors

--- colors.js
+++ colors.js
@@ -1,5 +1,11 @@
 const colors = {
   white: "#FFF",
+  "soft-green": "#DAFFF0",
+  green: "#31B380",
+  "soft-yellow": "#FFEBB9",
+  yellow: "#F3BF39",
+  "soft-red": "#FFEEED",
+  red: "#EB4132",
 };
 
 module.exports = colors;
