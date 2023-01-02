56e83a046c344da21f57053ae2f706b6c7ac9440
“naandalist” <listiananda.apriliawan@gmail.com>
2023-01-03 00:11:08 +0700

    feat: add state status for badge

---
+++ src/components/Badge/config.ts
@@ -0,0 +1,13 @@
+const configs: {
+  state: {
+    [key: string]: string;
+  };
+} = {
+  state: {
+    success: "bg-soft-green border-green text-green",
+    warning: "bg-soft-yellow border-yellow text-yellow",
+    danger: "bg-soft-red border-red text-red",
+  },
+};
+
+export default configs;
