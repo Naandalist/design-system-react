be05167c9bce628aff6e906ab582222ad07c4bfd
“naandalist” <listiananda.apriliawan@gmail.com>
2023-01-03 00:39:58 +0700

    fix: create example badge variant

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { ReactNode } from "react";
+import configs from "./config";
 
-type Props = {};
+type Props = {
+  children: ReactNode;
+  state: "success" | "warning" | "danger";
+  className?: string;
+};
 
-function Badge({}: Props) {
-  return <div>Badge</div>;
+function Badge({ children, state, className }: Props) {
+  const combineClassName = [
+    "py-1 px-4 rounded-full inline-flex items-center border border-green",
+    configs?.state?.[state] || "",
+    className || "",
+  ];
+  return <span className={combineClassName.join(" ")}>{children}</span>;
 }
+
+// Badge.defaultProps = {
+//   state: "success"
+// }
 
 export default Badge;
---
+++ src/stories/Badge/example/Sample.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Badge from "../../../components/Badge"
+
+type Props = {}
+
+const Sample = (props: Props) => {
+  return (
+    <Badge state="success">Success sample</Badge>
+  )
+}
+
+export default Sample;
