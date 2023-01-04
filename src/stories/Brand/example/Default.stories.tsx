6c6600b9c64a4afc8934b44cb2c60ceecc594253
“naandalist” <listiananda.apriliawan@gmail.com>
2023-01-04 12:04:41 +0700

    feat: add component brand

---
+++ src/stories/Brand/example/Default.stories.tsx
@@ -0,0 +1,29 @@
+// Badge.stories.ts|tsx
+
+import React from "react";
+
+import { ComponentMeta } from "@storybook/react";
+
+import Brand from "../../../components/Brand";
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Components/Brand/Example",
+  component: Brand,
+} as ComponentMeta<typeof Brand>;
+
+export const Dark = (args: any) => <div className="bg-green min-h-min w-full p-10"> <Brand className="w-32 h-16" {...args}/></div>;
+Dark.args = {
+    color: "dark"
+}
+
+export const Light = (args: any) =><div className="bg-navy min-h-min w-full p-10"> <Brand className="w-32 h-16" {...args}/></div> ;
+Light.args = {
+    color: "light"
+}
+
+
+
---
+++ src/stories/Brand/example/Sample.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Brand from "../../../components/Brand";
+
+type Props = {};
+
+const Sample = (props: Props) => {
+  return <Brand className="w-32 h-16" />;
+};
+
+export default Sample;
